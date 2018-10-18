8ce653f7f0bd5bfa3633895ff0699b0fd02af5f4
MrLaki5 <mimar1207@gmail.com>
2018-10-18 12:52:13 +0200

    c++ tank and player pawn added

---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+
+
+
+ATank* ATankPlayerController::GetControllerTank() const {
+	return Cast<ATank>(GetPawn());
+}
+
+
+void ATankPlayerController::BeginPlay() {
+	Super::BeginPlay();
+
+	auto ControlledTank = this->GetControllerTank();
+
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possesing a tank!"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing: %s"), *ControlledTank->GetName());
+	}
+}
